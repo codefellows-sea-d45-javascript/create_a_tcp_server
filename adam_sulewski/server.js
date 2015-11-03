f1ec1f4f449a5ae365f313581c5fc4b669a7d02f
adam-james <adam.j.sulewski@gmail.com>
2015-11-02 18:56:49 -0800

    Save request to file with unique timestamp name

--- adam_sulewski/server.js
+++ adam_sulewski/server.js
@@ -1,9 +1,23 @@
 'use strict';
 
+// CLOSE SOCKET!!!
+
 var net = require('net');
+var fs = require('fs');
+var dest = require(__dirname + '/lib/createFileName');
 
 var server = net.createServer(function(socket) {
-  socket.pipe(process.stdout);
+  socket.on('data', function(data) {
+    fs.writeFile(__dirname + '/log/' + dest() + '.json',
+      data.toString(), function(err) {
+        if (err) throw err;
+
+    });
+  });
+
+  socket.on('end', function() {
+    console.log('socket closed');
+  });
 });
 
 server.listen('3000', function() {
