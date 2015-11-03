9abaedbfb90f75f98260436041c61a6d603388b0
Kenji Crosland <kenji.crosland@gmail.com>
2015-11-03 13:54:01 -0800

    Remove superagent and use net.connect instead

--- kenji_crosland/tcp-server.js
+++ kenji_crosland/tcp-server.js
@@ -2,11 +2,10 @@
 'use strict';
 var net = require('net');
 var fs = require('fs');
-var request = require('superagent');
 
 var server = module.exports.server = net.createServer(function(socket){
   socket.on('data', function(data){
-    var requestString = "Requested on: " + new Date().toString() + "\n" + data.toString();
+    var requestString = data.toString();
     var writeStream = fs.createWriteStream("datalog.log", {'flags': 'a'});
     writeStream.write(requestString);
     writeStream.end();
@@ -21,5 +20,12 @@ var server = module.exports.server = net.createServer(function(socket){
 
 server.listen('3000', function(){
   console.log('server up');
-  request.get('http://localhost:3000').end(function(err, res){});
+  //var client = net.connect({port:3000});
+  //request.get('http://localhost:3000').end(function(err, res){});
+});
+
+var client = net.connect({port: 3000},
+    function() { //'connect' listener
+  console.log('connected to server!');
+  client.write("Requested on: " + new Date().toString());
 });
