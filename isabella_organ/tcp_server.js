f10ade7caa47769b2e586845db24c305a42b8d73
Isabella Organ <isabellaorgan@gmail.com>
2015-11-03 10:00:21 -0800

    server appears to be running, receiving files, and writing each to own unique file

---
+++ isabella_organ/tcp_server.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var net = require('net');
+var fs = require('fs');
+
+
+var server = net.createServer(function(socket) {
+	socket.on('data', function(data) {
+		fs.writeFile('uniquefile.log' + new Date().toString(), data.toString(), function(err) {
+			if (err) return console.log(err);
+		});
+		socket.write('some unique thing');
+		console.log('write to file');
+	});
+
+	socket.on('end', function() {
+		console.log('disconnected');
+		socket.end();
+	});
+});
+
+server.listen('3000', function() {
+	console.log('server up');
+});
+
+server.listen(3000);
+
+
---
+++ isabella_organ/test/test.js
@@ -0,0 +1,22 @@
+var fs = require('fs');
+var net = require('net');
+var request = require('superagent');
+var chai = require('chai');
+var chaiHttp: = require('chai-http');
+chai.use(chaiHttp);
+var expect = chai.expect;
+var server = require(__dirname + "/../tcp_server.js")
+
+
+describe('tcp server', function() {
+	it('should receive requests and save them to a file', function(done) {
+		chai.request('localhost:3000')
+		.get('/something')
+		.end(function(err, socket) {
+			expect(err).to.eql(null);
+			expect(socet.text).to.eql('some unique thing');
+			done();
+	});
+});
+
+
