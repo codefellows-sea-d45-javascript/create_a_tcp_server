7964ec2cf3ef238df3a1ea93bffd92085542b982
Tim Mendoza <timmydoza@gmail.com>
2015-11-03 11:49:36 -0800

    remove chai-http from test_tcplog.js per Tyler.

--- tim_mendoza/test/test_tcplog.js
+++ tim_mendoza/test/test_tcplog.js
@@ -1,28 +1,23 @@
 var mocha = require('mocha');
 var chai = require('chai');
 var expect = chai.expect;
-var chaiHttp = require('chai-http');
 var net = require('net');
 var fs = require('fs');
 var tcpLog = require(__dirname + '/../lib/tcplog');
-
-chai.use(chaiHttp);
 
 describe('newServer function', function() {
   it('should output a TCP request to file', function(done) {
     var file = __dirname + '/testfile.log';  
     var server = tcpLog.newServer('3000', file);
     var time = Date.now().toString();
+    var socket = net.connect(3000);
 
-    chai.request(server) 
-      .get('/test ' + time)
-      .end(function(err, res){});
-
+    socket.write('GET /test ' + time);
     server.on('finished', function() {
       fs.readFile(file, function(err, data) {
         if (err) throw err;
         var output = data.toString().split('\n');
-        expect(output[0]).to.equal('GET /test%20' + time + ' HTTP/1.1\r');
+        expect(output[0]).to.equal('GET /test ' + time);
         done();
       });
     });
