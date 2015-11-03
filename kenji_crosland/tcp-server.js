edc46bd5767f3b1f8502f2a43b0d65830dd68f0d
Kenji Crosland <kenji.crosland@gmail.com>
2015-11-03 10:27:16 -0800

    Tests complete and good to go!

--- kenji_crosland/tcp-server.js
+++ kenji_crosland/tcp-server.js
@@ -2,6 +2,7 @@
 'use strict';
 var net = require('net');
 var fs = require('fs');
+var request = require('superagent');
 
 var server = module.exports.server = net.createServer(function(socket){
   socket.on('data', function(data){
@@ -10,13 +11,16 @@ var server = module.exports.server = net.createServer(function(socket){
     var writeStream = fs.createWriteStream("datalog.log", {'flags': 'a'});
     writeStream.write(requestString);
     writeStream.end();
+    socket.end();
   });
 
   socket.on('end', function(){
     console.log('socket closed');
+    server.close();
   });
 });
 
 server.listen('3000', function(){
   console.log('server up');
+  request.get('http://localhost:3000').end(function(err, res){});
 });
--- kenji_crosland/test/test.js
+++ kenji_crosland/test/test.js
@@ -2,24 +2,29 @@ var fs = require('fs');
 var net = require('net');
 var expect = require('chai').expect;
 var server = require(__dirname + "/../tcp-server.js").server;
-var request = require('superagent');
 
 describe('the tcp server', function() {
+  var logstringBefore = "";
+  var logstringAfter = "";
 
-  beforeEach(function(){
-    server.listen('3000', function(){
-      console.log('server up in mocha!');
+  before(function(done){
+    fs.readFile('datalog.log', function(err, data){
+      logstringBefore = data.toString();
+      console.log(logstringBefore);
+      done();
     });
   });
 
-  it('should log all requests to a log file', function(done){
-  request.get('http://localhost:3000').end(function(err, res){
-    expect(true).to.equal(false); //This test doesn't run
-    //eventually write some fs code that tests if the log file is written to;
-
-
+  it('should log all requests to a log file and change it', function(done){
+    server.listen('3000', function(){
+    var requestString = "Requested on: " + new Date().toString();
+      console.log('server up in mocha!');
+      fs.readFile('datalog.log', function(err, data){
+        logstringAfter = data.toString();
+        expect(logstringAfter).to.include(requestString);
+        expect(logstringBefore).to.not.equal(logstringAfter);
+        done();
+       });
     });
-  //If I try server.close(), the log file isn't written to, and the 'data' event on the socket doesn't fire
-done();
   });
 });
