6ddb661159347debae2c325f7a889e1a9969bc65
Matthew Ringel <mattew.r.ringel@gmail.com>
2015-11-03 10:56:06 -0800

    chai-http test confirms file contents

--- matthew_ringel/test/tcp_test.js
+++ matthew_ringel/test/tcp_test.js
@@ -1,17 +1,33 @@
 var chai = require('chai');
 var chaiHttp = require('chai-http');
+var fs = require('fs');
 
 chai.use(chaiHttp);
 
 var appServer = 'http://localhost:3000';
 
 describe('an http request', function() {
-  it('should have a status of 200', function() {
+  // it('should have a status of 200', function() {
+  //   chai.request(appServer)
+  //     .get('/')
+  //     .then(function (res) {
+  //       console.log(res);
+  //       expect(res).to.have.status(200);
+  //     });
+  // });
+
+  it('should log a file containing "chai-test: true"', function() {
+    var newFile;
     chai.request(appServer)
-      .get('/')
-      .then(function (res) {
-        console.log(res);
-        expect(res).to.have.status(200);
+      .put('/test')
+      .set('chai-test', 'true')
+      .then(function (done) {
+        fs.watch(__dirname + '/../log/', function(event, filename) {
+          newFile = filename;
+          done();
+        });
+        var file = fs.readFileSync(String(newFile));
+        expect(file.toString()).to.contain('chai-test: true');
       });
   });
 });
