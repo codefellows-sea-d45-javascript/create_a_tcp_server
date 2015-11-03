3111dd5b98c839f9cd6c312e7890799be35ca6f9
Iryna Bondarenko <irusiabondarenko@gmail.com>
2015-11-03 14:39:39 -0800

    Test fixed

--- Iryna Bondarenko/test/test.js
+++ Iryna Bondarenko/test/test.js
@@ -1,10 +1,27 @@
 'use strict';
 
 var fs = require('fs');
-var expect = require('chai').expect;
+var chai = require('chai');
+var expect = chai.expect;
+var chaiHttp = require('chai-http');
 
-describe('TCP server', function () {
-	it("should create new unique file with post data", function () {
-		expect(fs.lstatSync('../*.txt').isFile().to.eql(true);
-	});
+chai.use(chaiHttp);
+
+describe('tcp server', function() {
+  var initialCount;
+  var newCount;
+
+ before(function(done) {
+   initialCount = fs.readdirSync(__dirname);
+    done();
+  });
+  it('should log request', function(done) {
+    chai.request('localhost:3000')
+    .get('/test')
+    .end(function(err, res) {
+      newCount = fs.readdirSync(__dirname);
+      expect(newCount.length).to.be.above(initialCount.length);
+    });
+    done();
+  });
 });
