26123d4e9b4b39d30788f7a758120e0a850503c3
Jack Sneed <sneed.jack@gmail.com>
2015-11-03 10:56:06 -0800

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var chai = require('chai');
+var fs = require('fs');
+var expect = chai.expect;
+var chaiHttp = require('chai-http');
+var server = require('../tcp_server');
+
+chai.use(chaiHttp);
+
+describe('tcp server', function() {
+  var initialCount;
+  var newCount;
+
+  before(function(done) {
+    initialCount = fs.readdirSync(__dirname + '/../log');
+    done();
+  });
+  it('should log request', function(done) {
+    chai.request('localhost:3000')
+    .get('/test')
+    .end(function(err, res) {
+      newCount = fs.readdirSync(__dirname + '/../log');
+      expect(newCount.length).to.be.above(initialCount.length);
+    });
+
+    done();
+  });
+});
