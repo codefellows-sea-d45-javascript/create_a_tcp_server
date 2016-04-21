5759ceee8460dfb993891327126581c63b995222
Isabella Organ <isabellaorgan@gmail.com>
2016-04-21 14:47:02 -0700

    update code style

--- isabella_organ/tcp_server.js
+++ isabella_organ/tcp_server.js
@@ -4,24 +4,21 @@ var net = require('net');
 var fs = require('fs');
 
 
-var server = net.createServer(function(socket) {
-	socket.on('data', function(data) {
-		fs.writeFile('uniquefile.log' + new Date().toString(), data.toString(), function(err) {
+var server = net.createServer((socket) => {
+	socket.on('data', (data) => {
+		fs.writeFile('uniquefile.log' + new Date().toString(), data.toString(), (err) => {
 			if (err) return console.log(err);
 		});
 		socket.write('some unique thing');
 		console.log('write to file');
 	});
 
-	socket.on('end', function() {
+	socket.on('end', () => {
 		console.log('disconnected');
 		socket.end();
 	});
 });
 
-server.listen('3000', function() {
+server.listen('3000', () => {
 	console.log('server up');
 });
-
-
-
